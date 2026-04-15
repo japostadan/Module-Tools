963f06684109490f292d1fa5d4be65a7eb960a7a
japostad <japostad@student.42barcelona.com>
2026-04-15 17:41:12 +0200

    Mod: remove static buffer and optimize for edge cases

--- implement-shell-tools/cat/cat.c
+++ implement-shell-tools/cat/cat.c
@@ -1,57 +1,72 @@
-/* Usage: ./cat [-n] [-b] file1 [file2 ...]
- * Compile: gcc -o cat cat.c
- */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/types.h>
 
-static void process_stream(FILE *f, int flag_n, int flag_b) {
-    int line_num = 0;
-    char line[8192];
-    while (fgets(line, sizeof(line), f)) {
-        int is_blank = (line[0] == '\n');
+static void process_stream(FILE *f, int flag_n, int flag_b, int *line_num) {
+    char *line = NULL;
+    size_t capacity = 0;
+    ssize_t len;
+
+    while ((len = getline(&line, &capacity, f)) != -1) {
+        int is_blank = (len == 1 && line[0] == '\n');
+
         if (flag_b) {
-            if (is_blank) {
-                fputs(line, stdout);
-            } else {
-                line_num++;
-                printf("%6d\t%s", line_num, line);
+            if (!is_blank) {
+                (*line_num)++;
+                fprintf(stdout, "%6d\t", *line_num);
             }
         } else if (flag_n) {
-            line_num++;
-            printf("%6d\t%s", line_num, line);
-        } else {
-            fputs(line, stdout);
+            (*line_num)++;
+            fprintf(stdout, "%6d\t", *line_num);
         }
+
+        fputs(line, stdout);
     }
+
+    free(line);
 }
 
 int main(int argc, char *argv[]) {
     int flag_n = 0, flag_b = 0;
-    char *files[256];
-    int nfiles = 0;
+    int exit_code = 0;
+    int line_num = 0;
+    int had_input = 0;
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-n") == 0)      flag_n = 1;
-        else if (strcmp(argv[i], "-b") == 0) flag_b = 1;
-        else files[nfiles++] = argv[i];
-    }
+        if (strcmp(argv[i], "-b") == 0) {
+            flag_b = 1;
+            flag_n = 0;
+        } else if (strcmp(argv[i], "-n") == 0) {
+            if (!flag_b)
+                flag_n = 1;
+        } else {
+            had_input = 1;
 
-    if (nfiles == 0) {
-        process_stream(stdin, flag_n, flag_b);
-        return 0;
-    }
+            FILE *f;
 
-    int exit_code = 0;
-    for (int i = 0; i < nfiles; i++) {
-        FILE *f = fopen(files[i], "r");
-        if (!f) {
-            fprintf(stderr, "cat: %s: %s\n", files[i], strerror(errno));
-            exit_code = 1;
-            continue;
+            if (strcmp(argv[i], "-") == 0) {
+                f = stdin;
+            } else {
+                f = fopen(argv[i], "r");
+                if (!f) {
+                    fprintf(stderr, "cat: %s: %s\n", argv[i], strerror(errno));
+                    exit_code = 1;
+                    continue;
+                }
+            }
+
+            process_stream(f, flag_n, flag_b, &line_num);
+
+            if (f != stdin)
+                fclose(f);
         }
-        process_stream(f, flag_n, flag_b);
-        fclose(f);
     }
+
+    if (!had_input) {
+        process_stream(stdin, flag_n, flag_b, &line_num);
+    }
+
     return exit_code;
 }
