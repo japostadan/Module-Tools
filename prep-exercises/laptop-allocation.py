75aa12d686557dcf2d9f5fdf950f6bdb9c29c3a2
japostad <japostad@student.42barcelona.com>
2026-04-28 01:17:39 +0200

    Add: init exercise laptop allocation

---
+++ prep-exercises/laptop-allocation.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+class OperatingSystem(Enum):
+    MACOS = "macOS"
+    ARCH = "Arch Linux"
+    UBUNTU = "Ubuntu"
+
+@dataclass(frozen=True)
+class Person:
+    name: str
+    age: int
+    # Sorted in order of preference, most preferred is first.
+    preferred_operating_system: List[OperatingSystem]
+
+
+@dataclass(frozen=True)
+class Laptop:
+    id: int
+    manufacturer: str
+    model: str
+    screen_size_in_inches: float
+    operating_system: OperatingSystem
